7038a80b2557aa972fa95022be0fc5b84c1bb7f4
Eloyd Lee Gonzales <egonzales164@cnm.edu>
2024-10-01 22:53:25 -0600

    Add morse to text function

--- source/morseCodeTranslator.cpp
+++ source/morseCodeTranslator.cpp
@@ -2,8 +2,7 @@
 #include <getopt.h>
 #include <string>
 #include <fstream>
-
-using namespace std;
+#include <sstream>
 
 // Implement functions as needed to:
 // Convert a string of text to Morse code
@@ -26,164 +25,177 @@ using namespace std;
 // Save the output to a file
 // Implement error handling for invalid inputs
 
+const std::string MORSE_TABLE[128]{
+    "",		// 0	NUL
+    "",		// 1	SOH
+    "",		// 2	STX
+    "",		// 3	ETX
+    "",		// 4	EOT
+    "",		// 5	ENQ
+    "",		// 6	ACK
+    "",		// 7	BEL
+    "",		// 8	BS
+    "",		// 9	HT
+    ".-.-",		// 10	LF
+    "",		// 11	VT
+    "",		// 12	FF
+    "",		// 13	CR
+    "",		// 14	SO
+    "",		// 15	SI
+    "",		// 16	DLE
+    "",		// 17	DC1
+    "",		// 18	DC2
+    "",		// 19	DC3
+    "",		// 20	DC4
+    "",		// 21	NAK
+    "",		// 22	SYN
+    "",		// 23	ETB
+    "",		// 24	CAN
+    "",		// 25	EM
+    "",		// 26	SUB
+    "",		// 27	ESC
+    "",		// 28	FS
+    "",		// 29	GS
+    "",		// 30	RS
+    "",		// 31	US
+    "/",		// 32	SP
+    "-.-.--",		// 33	!
+    ".-..-.",		// 34	"
+    "",		// 35	#
+    "",		// 36	$
+    "",		// 37	%
+    ".-...",		// 38	&
+    ".----.",		// 39	'
+    "-.--.",		// 40	(
+    "-.--.-",		// 41	)
+    "",		// 42	*
+    ".-.-.",		// 43	+
+    "--..--",		// 44	,
+    "-....-",		// 45	-
+    ".-.-.-",		// 46	.
+    "-..-.",		// 47	/
+    "-----",		// 48	0
+    ".----",		// 49	1
+    "..---",		// 50	2
+    "...--",		// 51	3
+    "....-",		// 52	4
+    ".....",		// 53	5
+    "-....",		// 54	6
+    "--...",		// 55	7
+    "---..",		// 56	8
+    "----.",		// 57	9
+    "---...",		// 58	:
+    "",		// 59	;
+    "",		// 60	<
+    "-...-",		// 61	=
+    "",		// 62	>
+    "..--..",		// 63	?
+    ".--.-.",		// 64	@
+    ".-",		// 65	A
+    "-...",		// 66	B
+    "-.-.",		// 67	C
+    "-..",		// 68	D
+    ".",		// 69	E
+    "..-.",		// 70	F
+    "--.",		// 71	G
+    "....",		// 72	H
+    "..",		// 73	I
+    ".---",		// 74	J
+    "-.-",		// 75	K
+    ".-..",		// 76	L
+    "--",		// 77	M
+    "-.",		// 78	N
+    "---",		// 79	O
+    ".--.",		// 80	P
+    "--.-",		// 81	Q
+    ".-.",		// 82	R
+    "...",		// 83	S
+    "-",		// 84	T
+    "..-",		// 85	U
+    "...-",		// 86	V
+    ".--",		// 87	W
+    "-..-",		// 88	X
+    "-.--",		// 89	Y
+    "--..",		// 90	Z
+    "",		// 91	[
+    "",		// 92   "\"
+    "",		// 93	]
+    "",		// 94	^
+    "",		// 95	_
+    "",		// 96	`
+    ".-",
+    "-...",
+    "-.-.",
+    "-..",
+    ".",
+    "..-.",
+    "--.",
+    "....",
+    "..",
+    ".---",
+    "-.-",
+    ".-..",
+    "--",
+    "-.",
+    "---",
+    ".--.",
+    "--.-",
+    ".-.",
+    "...",
+    "-",
+    "..-",
+    "...-",
+    ".--",
+    "-..-",
+    "-.--",
+    "--..",
+    "",		// 123	{
+    "",		// 124	|
+    "",		// 125	}
+    "",		// 126	~
+    ""		// 127	DEL
+};
 
 std::string textToMorse(char c)
 {
-
-    std::string morseTable[128]{
-        "",		// 0	NUL
-        "",		// 1	SOH
-        "",		// 2	STX
-        "",		// 3	ETX
-        "",		// 4	EOT
-        "",		// 5	ENQ
-        "",		// 6	ACK
-        "",		// 7	BEL
-        "",		// 8	BS
-        "",		// 9	HT
-        ".-.-",		// 10	LF
-        "",		// 11	VT
-        "",		// 12	FF
-        "",		// 13	CR
-        "",		// 14	SO
-        "",		// 15	SI
-        "",		// 16	DLE
-        "",		// 17	DC1
-        "",		// 18	DC2
-        "",		// 19	DC3
-        "",		// 20	DC4
-        "",		// 21	NAK
-        "",		// 22	SYN
-        "",		// 23	ETB
-        "",		// 24	CAN
-        "",		// 25	EM
-        "",		// 26	SUB
-        "",		// 27	ESC
-        "",		// 28	FS
-        "",		// 29	GS
-        "",		// 30	RS
-        "",		// 31	US
-        "/",		// 32	SP
-        "-.-.--",		// 33	!
-        ".-..-.",		// 34	"
-        "",		// 35	#
-        "",		// 36	$
-        "",		// 37	%
-        ".-...",		// 38	&
-        ".----.",		// 39	'
-        "-.--.",		// 40	(
-        "-.--.-",		// 41	)
-        "",		// 42	*
-        ".-.-.",		// 43	+
-        "--..--",		// 44	,
-        "-....-",		// 45	-
-        ".-.-.-",		// 46	.
-        "-..-.",		// 47	/
-        "-----",		// 48	0
-        ".----",		// 49	1
-        "..---",		// 50	2
-        "...--",		// 51	3
-        "....-",		// 52	4
-        ".....",		// 53	5
-        "-....",		// 54	6
-        "--...",		// 55	7
-        "---..",		// 56	8
-        "----.",		// 57	9
-        "---...",		// 58	:
-        "",		// 59	;
-        "",		// 60	<
-        "-...-",		// 61	=
-        "",		// 62	>
-        "..--..",		// 63	?
-        ".--.-.",		// 64	@
-        ".-",		// 65	A
-        "-...",		// 66	B
-        "-.-.",		// 67	C
-        "-..",		// 68	D
-        ".",		// 69	E
-        "..-.",		// 70	F
-        "--.",		// 71	G
-        "....",		// 72	H
-        "..",		// 73	I
-        ".---",		// 74	J
-        "-.-",		// 75	K
-        ".-..",		// 76	L
-        "--",		// 77	M
-        "-.",		// 78	N
-        "---",		// 79	O
-        ".--.",		// 80	P
-        "--.-",		// 81	Q
-        ".-.",		// 82	R
-        "...",		// 83	S
-        "-",		// 84	T
-        "..-",		// 85	U
-        "...-",		// 86	V
-        ".--",		// 87	W
-        "-..-",		// 88	X
-        "-.--",		// 89	Y
-        "--..",		// 90	Z
-        "",		// 91	[
-        "",		// 92   "\"
-        "",		// 93	]
-        "",		// 94	^
-        "",		// 95	_
-        "",		// 96	`
-                ".-",
-        "-...",
-        "-.-.",
-        "-..",
-        ".",
-        "..-.",
-        "--.",
-        "....",
-        "..",
-        ".---",
-        "-.-",
-        ".-..",
-        "--",
-        "-.",
-        "---",
-        ".--.",
-        "--.-",
-        ".-.",
-        "...",
-        "-",
-        "..-",
-        "...-",
-        ".--",
-        "-..-",
-        "-.--",
-        "--..",
-        "",		// 123	{
-        "",		// 124	|
-        "",		// 125	}
-        "",		// 126	~
-        ""		// 127	DEL
-    };
-    // if (!encrypt)
-    // {
-    //     shift = shift * -1;
-    // }
-    std::cout << (int)c << " " ;
-    if (morseTable[(int)c] != "")
+    // std::cout << (int)c << " " ;
+    if (MORSE_TABLE[(int)c] != "")
     {
-        std::cout << c << " = " << morseTable[(int)c] << std::endl;
-        return morseTable[(int)c];
+        // std::cout << c << " = " << MORSE_TABLE[(int)c] << std::endl;
+        return MORSE_TABLE[(int)c];
     }
     else
     {
-        // std::string foo = str(c);
-        // return str(c);
-        std::cout << c << " = " << morseTable[(int)c] << std::endl;
+        // std::cout << c << " = " << MORSE_TABLE[(int)c] << std::endl;
         return std::string(1, c);
     }
     return "ERROR";
 }
 
+char morseToText(std::string s)
+{
+    // std::cout << s << " = ";
+    
+    for (size_t i = 0; i < 128; i++)
+    {
+        if (MORSE_TABLE[i] == s)
+        {
+            // std::cout << char(i) << std::endl;
+            return char(i);
+        }
+        
+    }
+    
+    std::cout << "Not found" << std::endl;
+}
+
 int main(int argc, char* argv[])
 {
     int opt{-1};
-    bool verbose{false}, interactive{false};
+    bool verbose{false}, 
+        interactive{false}, 
+        toText{false}, 
+        toMorse{false};
     std::string source{""}, destination{""};
 
     // Define long options alongside short options
@@ -195,11 +207,12 @@ int main(int argc, char* argv[])
         {"source_file", required_argument,       0, 's'},
         {"out_file",    required_argument,       0, 'o'},
         {"to_text",            no_argument,       0, 't'},
+        {"to_morse",            no_argument,       0, 'm'},
         {0,         0,                 0,  0 }
     };
 
     // Parse options using getopt_long
-    while ((opt = getopt_long(argc, argv, "hvisot", long_options, nullptr)) != -1)
+    while ((opt = getopt_long(argc, argv, "hvisotm", long_options, nullptr)) != -1)
     {
         switch (opt)
         {
@@ -207,7 +220,8 @@ int main(int argc, char* argv[])
                 std::cout << "Usage: mctranslator [options] SOURCE DEST\n"
                           << "Options:\n"
                           << "  -h, --help     Show this help message\n"
-                          << "TODO\n";
+                          << "  -m  --to_morse Translates text into morse code\n"
+                          << "  -t  --to_text  Translates morse code into text";
                 return 0;
             case 'v':
                 verbose = true;
@@ -222,7 +236,10 @@ int main(int argc, char* argv[])
                 
                 break;
             case 't':
-                
+                toText = true;
+                break;
+            case 'm':
+                toMorse = true;
                 break;
             default:
                 std::cerr << "Unknown option: " << opt << std::endl;
@@ -240,29 +257,78 @@ int main(int argc, char* argv[])
         
     }
 
-    std::cout << "source=" << source << std::endl;
-    std::cout << "destination=" << destination << std::endl;
-
+    if (verbose)
+    {
+        std::cout << "source=" << source << std::endl;
+        std::cout << "destination=" << destination << std::endl;
+    }
+    
     std::ifstream inputFile (source);
     std::ofstream outputFile (destination);
 
-    if (inputFile.is_open() && outputFile.is_open())
+    if (toMorse)
     {
-        std::string line;
-        while ( getline (inputFile,line) )
+        if (inputFile.is_open() && outputFile.is_open())
         {
-            for ( std::string::iterator it=line.begin(); it!=line.end(); ++it)
+            std::string line;
+            while ( getline (inputFile,line) )
             {
-                std::cout << (int)*it << " = " << *it << "    ";
-                outputFile << textToMorse(*it);
+                for ( std::string::iterator it=line.begin(); it!=line.end(); ++it)
+                {
+                    if (verbose)
+                    {
+                        std::cout << textToMorse(*it) << " ";
+                    }
+                    outputFile << textToMorse(*it) << " ";
+                }
+                if (verbose)
+                {
+                    std::cout << std::endl;
+                }
+                outputFile << std::endl;
             }
-            outputFile << std::endl;
+            inputFile.close();
+            outputFile.close();
         }
-        inputFile.close();
-        outputFile.close();
-    }
-    else std::cout << "Unable to open file";
+        else std::cout << "Unable to open file";
 
+        return 0;
+    }
+
+    if (toText)
+    {
+        std::stringstream ss;
+        if (inputFile.is_open() && outputFile.is_open())
+        {
+            std::string line, letter;
+            while ( getline (inputFile,line) )
+            {
+                ss << line;
+                while ( getline (ss,letter, ' ') )
+                {
+                    if (verbose)
+                    {
+                        std::cout << morseToText(letter);
+                    }
+                    outputFile << morseToText(letter);
+                }
+                ss.clear();
+                ss.str(std::string());
+
+                if (verbose)
+                {
+                    std::cout << std::endl;
+                }
+                outputFile << std::endl;
+            }
+            inputFile.close();
+            outputFile.close();
+        }
+        else std::cout << "Unable to open file";
+
+        return 0;
+    }
+    
 
 
     return 0;
